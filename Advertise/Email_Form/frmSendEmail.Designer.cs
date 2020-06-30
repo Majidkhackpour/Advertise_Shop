db5519a640ed3754a65b89a89fa02810c6003b45
arad_enj <arad_enj@yahoo.com>
2020-06-30 16:27:58 +0430

    sync

---
+++ Advertise/Email_Form/frmSendEmail.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Advertise.Email_Form
+{
+    partial class frmSendEmail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSendEmail));
+            this.grpAccount = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.label17 = new System.Windows.Forms.Label();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtReciever = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.uC_Date1 = new UC_Date.UC_Date();
+            this.btnCancel = new DevComponents.DotNetBar.ButtonX();
+            this.btnFinish = new DevComponents.DotNetBar.ButtonX();
+            this.line1 = new DevComponents.DotNetBar.Controls.Line();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.grpAccount.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpAccount
+            // 
+            this.grpAccount.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpAccount.BackColor = System.Drawing.Color.White;
+            this.grpAccount.CanvasColor = System.Drawing.SystemColors.Control;
+            this.grpAccount.Controls.Add(this.label17);
+            this.grpAccount.Controls.Add(this.txtDesc);
+            this.grpAccount.Controls.Add(this.label1);
+            this.grpAccount.Controls.Add(this.txtSubject);
+            this.grpAccount.Controls.Add(this.txtReciever);
+            this.grpAccount.Controls.Add(this.label2);
+            this.grpAccount.DisabledBackColor = System.Drawing.Color.Empty;
+            this.grpAccount.Location = new System.Drawing.Point(12, 50);
+            this.grpAccount.Name = "grpAccount";
+            this.grpAccount.Size = new System.Drawing.Size(780, 492);
+            // 
+            // 
+            // 
+            this.grpAccount.Style.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(61)))));
+            this.grpAccount.Style.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(61)))));
+            this.grpAccount.Style.BackColorGradientAngle = 90;
+            this.grpAccount.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.grpAccount.Style.BorderBottomWidth = 3;
+            this.grpAccount.Style.BorderColor = System.Drawing.Color.Silver;
+            this.grpAccount.Style.BorderColor2 = System.Drawing.Color.Silver;
+            this.grpAccount.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.grpAccount.Style.BorderLeftWidth = 3;
+            this.grpAccount.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.grpAccount.Style.BorderRightWidth = 3;
+            this.grpAccount.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.grpAccount.Style.BorderTopWidth = 3;
+            this.grpAccount.Style.CornerDiameter = 4;
+            this.grpAccount.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.grpAccount.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.grpAccount.Style.TextColor = System.Drawing.Color.Black;
+            this.grpAccount.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            // 
+            // 
+            // 
+            this.grpAccount.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.grpAccount.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.grpAccount.TabIndex = 1;
+            // 
+            // label17
+            // 
+            this.label17.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label17.BackColor = System.Drawing.Color.Transparent;
+            this.label17.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label17.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label17.ForeColor = System.Drawing.Color.Silver;
+            this.label17.Location = new System.Drawing.Point(634, 64);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(138, 20);
+            this.label17.TabIndex = 35;
+            this.label17.Text = "موضوع";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDesc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
+            this.txtDesc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDesc.ForeColor = System.Drawing.Color.Silver;
+            this.txtDesc.Location = new System.Drawing.Point(3, 145);
+            this.txtDesc.MaxLength = 30000;
+            this.txtDesc.Multiline = true;
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDesc.Size = new System.Drawing.Size(765, 338);
+            this.txtDesc.TabIndex = 2;
+            this.txtDesc.Enter += new System.EventHandler(this.txtDesc_Enter);
+            this.txtDesc.Leave += new System.EventHandler(this.txtDesc_Leave);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(620, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "متن ایمیل";
+            // 
+            // txtReciever
+            // 
+            this.txtReciever.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtReciever.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
+            this.txtReciever.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtReciever.ForeColor = System.Drawing.Color.Silver;
+            this.txtReciever.Location = new System.Drawing.Point(407, 33);
+            this.txtReciever.MaxLength = 3000;
+            this.txtReciever.Name = "txtReciever";
+            this.txtReciever.Size = new System.Drawing.Size(361, 27);
+            this.txtReciever.TabIndex = 0;
+            this.txtReciever.Enter += new System.EventHandler(this.txtReciever_Enter);
+            this.txtReciever.Leave += new System.EventHandler(this.txtReciever_Leave);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.ForeColor = System.Drawing.Color.Silver;
+            this.label2.Location = new System.Drawing.Point(620, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 20);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "گیرنده";
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHeader.Font = new System.Drawing.Font("B Yekan", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblHeader.ForeColor = System.Drawing.Color.Silver;
+            this.lblHeader.Location = new System.Drawing.Point(607, 5);
+            this.lblHeader.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(180, 24);
+            this.lblHeader.TabIndex = 55708;
+            this.lblHeader.Text = "ارسال ایمیل";
+            // 
+            // uC_Date1
+            // 
+            this.uC_Date1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(61)))));
+            this.uC_Date1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.uC_Date1.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.uC_Date1.Location = new System.Drawing.Point(0, 0);
+            this.uC_Date1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uC_Date1.Name = "uC_Date1";
+            this.uC_Date1.Size = new System.Drawing.Size(800, 47);
+            this.uC_Date1.TabIndex = 55707;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(33)))));
+            this.btnCancel.ColorTable = DevComponents.DotNetBar.eButtonColor.Flat;
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnCancel.Location = new System.Drawing.Point(12, 568);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Shape = new DevComponents.DotNetBar.RoundRectangleShapeDescriptor(14);
+            this.btnCancel.Size = new System.Drawing.Size(139, 25);
+            this.btnCancel.Style = DevComponents.DotNetBar.eDotNetBarStyle.VS2005;
+            this.btnCancel.TabIndex = 55711;
+            this.btnCancel.Text = "انصراف (ESC)";
+            this.btnCancel.TextColor = System.Drawing.Color.Silver;
+            this.btnCancel.ThemeAware = true;
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnFinish.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFinish.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(33)))));
+            this.btnFinish.ColorTable = DevComponents.DotNetBar.eButtonColor.Flat;
+            this.btnFinish.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFinish.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnFinish.Location = new System.Drawing.Point(637, 568);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Shape = new DevComponents.DotNetBar.RoundRectangleShapeDescriptor(14);
+            this.btnFinish.Size = new System.Drawing.Size(155, 25);
+            this.btnFinish.Style = DevComponents.DotNetBar.eDotNetBarStyle.VS2005;
+            this.btnFinish.TabIndex = 55710;
+            this.btnFinish.Text = "تایید (F5)";
+            this.btnFinish.TextColor = System.Drawing.Color.Silver;
+            this.btnFinish.ThemeAware = true;
+            this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click);
+            // 
+            // line1
+            // 
+            this.line1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.line1.ForeColor = System.Drawing.Color.Silver;
+            this.line1.Location = new System.Drawing.Point(0, 548);
+            this.line1.Name = "line1";
+            this.line1.Size = new System.Drawing.Size(811, 19);
+            this.line1.TabIndex = 55712;
+            this.line1.Text = "line1";
+            this.line1.Thickness = 2;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSubject.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
+            this.txtSubject.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSubject.ForeColor = System.Drawing.Color.Silver;
+            this.txtSubject.Location = new System.Drawing.Point(3, 92);
+            this.txtSubject.MaxLength = 3000;
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(765, 27);
+            this.txtSubject.TabIndex = 0;
+            this.txtSubject.Enter += new System.EventHandler(this.txtSubject_Enter);
+            this.txtSubject.Leave += new System.EventHandler(this.txtSubject_Leave);
+            // 
+            // frmSendEmail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(61)))));
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.line1);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.uC_Date1);
+            this.Controls.Add(this.grpAccount);
+            this.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmSendEmail";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmSendEmail_KeyDown);
+            this.grpAccount.ResumeLayout(false);
+            this.grpAccount.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public DevComponents.DotNetBar.Controls.GroupPanel grpAccount;
+        public System.Windows.Forms.Label label17;
+        public System.Windows.Forms.TextBox txtDesc;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtReciever;
+        public System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblHeader;
+        private UC_Date.UC_Date uC_Date1;
+        public System.Windows.Forms.TextBox txtSubject;
+        public DevComponents.DotNetBar.ButtonX btnCancel;
+        public DevComponents.DotNetBar.ButtonX btnFinish;
+        private DevComponents.DotNetBar.Controls.Line line1;
+    }
+}
---
+++ Advertise/Email_Form/frmSendEmail.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Forms;
+using Advertise.Notification_Form;
+using PacketParser.Services;
+
+namespace Advertise.Email_Form
+{
+    public partial class frmSendEmail : Form
+    {
+        public frmSendEmail(string reciever)
+        {
+            InitializeComponent();
+            txtReciever.Text = reciever;
+        }
+
+        private void txtReciever_Enter(object sender, System.EventArgs e)
+        {
+            txtSetter.Focus(txtReciever);
+        }
+
+        private void txtSubject_Enter(object sender, System.EventArgs e)
+        {
+            txtSetter.Focus(txtSubject);
+        }
+
+        private void txtDesc_Enter(object sender, System.EventArgs e)
+        {
+            txtSetter.Focus(txtDesc);
+        }
+
+        private void txtDesc_Leave(object sender, System.EventArgs e)
+        {
+            txtSetter.Follow(txtDesc);
+        }
+
+        private void txtSubject_Leave(object sender, System.EventArgs e)
+        {
+            txtSetter.Follow(txtSubject);
+        }
+
+        private void txtReciever_Leave(object sender, System.EventArgs e)
+        {
+            txtSetter.Follow(txtReciever);
+        }
+
+        private void frmSendEmail_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Enter:
+                        if (!btnFinish.Focused && !btnCancel.Focused)
+                            SendKeys.Send("{Tab}");
+                        break;
+                    case Keys.F5:
+                        btnFinish.PerformClick();
+                        break;
+                    case Keys.Escape:
+                        btnCancel.PerformClick();
+                        break;
+                }
+            }
+            catch (Exception exception)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(exception);
+            }
+        }
+
+        private void btnFinish_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtReciever.Text))
+                {
+                    frmNotification.PublicInfo.ShowMessage("ایمیل گیرنده نمی تواند خالی باشد");
+                    txtReciever.Focus();
+                    return;
+                }
+                if (string.IsNullOrEmpty(txtSubject.Text))
+                {
+                    frmNotification.PublicInfo.ShowMessage("موضوع ایمیل نمی تواند خالی باشد");
+                    txtSubject.Focus();
+                    return;
+                }
+                if (string.IsNullOrEmpty(txtDesc.Text))
+                {
+                    frmNotification.PublicInfo.ShowMessage("متن ایمیل نمی تواند خالی باشد");
+                    txtDesc.Focus();
+                    return;
+                }
+
+
+                SendEmail.Send(txtReciever.Text, txtSubject.Text, txtDesc.Text);
+
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+        }
+    }
+}
